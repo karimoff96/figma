402b90a1444c274ada2a7b9c8b3061f01c3203e3
doniyor <doniyorkarimoff96@gmail.com>
2022-02-25 17:15:05 +0500

    deploying

--- orders/views.py
+++ orders/views.py
@@ -4,11 +4,15 @@ from cart.cart import Cart
 from django.urls import reverse
 from django.shortcuts import render, redirect
 
+
 def order_create(request):
     cart = Cart(request)
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
+            orders = OrderItem.objects.all()
+            # total = orders.get(price='price', quantity='quantity')
+            # all = int(total.price)*int(total.quantity)
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(order=order,
@@ -17,7 +21,7 @@ def order_create(request):
                                          quantity=item['quantity'])
             # clear the cart
             cart.clear()
-            return render(request, 'orders/order/created.html', {'order': order})
+            return render(request, 'orders/order/created.html', {'order': order, 'orders': orders})
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
